28d1e49fdc302eb1c3f139c0499bc535687893f3
Hieu <hieun201104@gmail.com>
2024-11-15 18:06:06 -0800

    help

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,12 @@
-<<<<<<< HEAD
-import { Route, Routes } from "react-router-dom"
-import Home from "./Pages/Home/Home"
+import { Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
 
-=======
-import Navbar from "./Components/Navbar";
-import LandingPage from "./Components/LandingPage";
->>>>>>> 56c20cd (Feat: Created Simple Landing Page for #2)
 function App() {
   return (
-<<<<<<< HEAD
     <Routes>
       <Route path="/new-site" element={<Home />} />
     </Routes>
-  )
-=======
-    <>
-      <Navbar />
-      <LandingPage />
-    </>
   );
->>>>>>> 56c20cd (Feat: Created Simple Landing Page for #2)
 }
 
 export default App;
--- src/Components/Navbar.tsx
+++ src/Components/Navbar.tsx
@@ -1,4 +1,3 @@
-// import { FaHome } from "react-icons/fa";
 import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
