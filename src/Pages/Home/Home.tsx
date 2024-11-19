65eafbac37a6b1607470cecf38223afec79d9115
Issac Roy <issacroy05@gmail.com>
2024-11-19 10:41:07 -0800

    Refactor: Renamed events section to where we've been

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -1,6 +1,6 @@
 import Page from "../../Components/Page/Page";
 import AboutUs from "./AboutUs";
-import Events from "./Events";
+import Events from "./WhereWeBeen";
 import Landing from "./Landing";
 import Sponsers from "./Sponsers";
 import Stats from "./Stats";
--- src/Pages/Home/WhereWeBeen.tsx
+++ src/Pages/Home/WhereWeBeen.tsx
@@ -1,8 +1,8 @@
-import Carousel from "../../Components/Events/Carousel";
+import Carousel from "../../Components/WhereWeBeen/Carousel";
 const Events = () => {
   return (
     <div className="flex flex-col items-center gap-10 w-[80vw] py-24">
-      <div className="text-6xl text-white">Explore Our Events</div>
+      <div className="text-6xl text-white">Where We've Been</div>
       <div className="w-full text-[6vh]">
         <Carousel />
       </div>
