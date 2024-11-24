96c58d6455618e2bd07f7f44c1741d9ef7d15a59
Hieu <hieun201104@gmail.com>
2024-11-23 18:26:42 -0800

    Feat: button media queries

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -15,9 +15,12 @@ const Button = ({
   contents: string;
 }) => {
   return (
-    <div className="relative w-[10vw] h-[3vw] mt-4" onClick={onclick}>
+    <div
+      className="relative md:w-[10vw] md:h-[3vw] w-[25vw] h-[7vw] mt-4"
+      onClick={onclick}
+    >
       <motion.button
-        className="absolute top-0 w-full h-full border-2 rounded-full p-[0.5vw] bg-black text-white text-[1.2vw]"
+        className="absolute top-0 w-full h-full border-2 rounded-full p-[0.5vw] bg-black text-white md:text-[1.2vw] text-[4vw]"
         variants={buttonVariants}
         initial="initial"
         animate="initial"
@@ -25,7 +28,7 @@ const Button = ({
       >
         {contents}
       </motion.button>
-      <div className="absolute top-0 translate-y-[0.5vw]  border-2 w-full h-full rounded-full bg-white z-[-1]"></div>
+      <div className="absolute top-0 translate-y-[1.2vw] md:translate-y-[0.5vw]  border-2 w-full h-full rounded-full bg-white z-[-1]"></div>
     </div>
   );
 };
--- src/Components/Landing/TextArea.tsx
+++ src/Components/Landing/TextArea.tsx
@@ -8,13 +8,13 @@ const Text = forwardRef((_props, ref: LegacyRef<HTMLDivElement>) => {
       ref={ref}
     >
       <div className="">
-        <div className=" text-[4vw] text-white">DS3</div>
-        <div className="flex text-white text-[5vw] leading-[4vw] ml-[-0.1rem]">
+        <div className=" lg:text-[4vw] text-white">DS3</div>
+        <div className="flex text-white lg:text-[5vw] leading-[4vw] ml-[-0.1rem] text-[6vw]">
           <div className="text-[#F58134]">Learn</div>,
           <div className="text-[#19B5CA] ml-[0.5rem]">Build</div>,
           <div className="text-[#A9A9A9] ml-[0.5rem]">Innovate</div>, With Data
         </div>
-        <div className="mt-4 text-[1.5vw]">
+        <div className="mt-4 md:text-[1.5vw] text-[4vw] sm:text-[3vw]">
           We are here to expand the horizons of data science as a community
           together.
         </div>
