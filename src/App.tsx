1ef9ad908f8e6f69844d0817657a15dccbcdeada
anagha166 <anaghakamath@gmail.com>
2025-03-03 19:36:03 -0800

    Fix: Updated routing to link nav to join us & partners

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,15 @@ import Home from "./Pages/Home/Home";
 import Datahacks from "./Pages/Datahacks/Datahacks";
 import Alumni from "./Pages/OurTeam/Alumni/Alumni";
 import Board from "./Pages/OurTeam/Board/Board";
+import Partners from "./Pages/Partners/PartnersPage";
+import JoinUs from "./Pages/JoinUs/JoinUsPage";
 
 function App() {
   return (
     <Routes>
       <Route path="" element={<Home />} />
+      <Route path="/partners" element={<Partners />} />
+      <Route path="/join" element={<JoinUs />} />
       <Route path="events">
         <Route path="datahacks" element={<Datahacks />} />
       </Route>
