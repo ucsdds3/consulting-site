e169093f9669eadf278cb733d5483d746edf9d56
Issac Roy <issacroy05@gmail.com>
2025-03-31 20:15:07 -0700

    Bolded navbar buttons

--- src/Components/Page/Navbar.tsx
+++ src/Components/Page/Navbar.tsx
@@ -17,9 +17,9 @@ const Navbar = () => {
         {/* <Switch className="hover:cursor-pointer" /> */}
       </div>
       <div className="flex flex-1 justify-end mr-2 gap-4">
-        <Button variant={"ghost"} className="text-white hover:cursor-pointer">About Us</Button>
-        <Button variant={"ghost"} className="text-white hover:cursor-pointer">Our Work</Button>
-        <Button variant={"ghost"} className="text-white hover:cursor-pointer">Contact Us</Button>
+        <Button variant={"ghost"} className="text-white font-bold hover:cursor-pointer">About Us</Button>
+        <Button variant={"ghost"} className="text-white font-bold hover:cursor-pointer">Our Work</Button>
+        <Button variant={"ghost"} className="text-white font-bold hover:cursor-pointer">Contact Us</Button>
         <Button variant={"ghost"} className="bg-[#F58134] text-black font-bold hover:cursor-pointer">DS3 Main Site</Button>
       </div>
     </div>
