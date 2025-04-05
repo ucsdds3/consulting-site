aeb85921349643f6d7088e0b6916a6bbfa0dc5ce
Hieu <hieun201104@gmail.com>
2025-04-04 21:04:24 -0700

    Feat: Where we been responsiveness

--- src/Components/WhereWeBeen.tsx
+++ src/Components/WhereWeBeen.tsx
@@ -2,14 +2,17 @@ import Google from "/src/Assets/Google.png";
 
 const WhereWeBeen = () => {
   return (
-    <div className="flex p-10 px-24 min-h-[80vh] w-full gap-[10vw]">
+    <div className="flex p-10 px-5 md:px-24 min-h-[80vh] w-full gap-[10vw] flex-wrap">
       <div className="flex-4 flex flex-col justify-center gap-4">
-        <h2 className="text-[3vw] font-bold text-white">
+        <h2 className="text-[clamp(32px,5vw,50px)] font-bold text-white">
           Our team has gone on to work for numerous companies.
         </h2>
-        <p className="text-[1vw] text-neutral-300">We are here to expand the horizons of data science as a community together.</p>
+        <p className="text-[clamp(16px,2.5vw,25px)] text-neutral-300">
+          We are here to expand the horizons of data science as a community
+          together.
+        </p>
       </div>
-      <div className="flex-3 grid grid-cols-3 grid-rows-3 gap-x-10 place-items-center py-24">
+      <div className="flex-3 min-w-[320px] grid grid-cols-3 grid-rows-3 gap-x-10 place-items-center md:py-24">
         {Array.from({ length: 9 }).map((_, index) => (
           <img src={Google} key={index} className="w-full" alt="" />
         ))}
