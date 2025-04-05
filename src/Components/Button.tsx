d4ae9812310417ef57f422703af530233ed8c211
Hieu <hieun201104@gmail.com>
2025-04-04 20:13:55 -0700

    Fix: Button maintains size at all window sizes

--- src/Components/Button.tsx
+++ src/Components/Button.tsx
@@ -3,17 +3,17 @@ import { motion } from "framer-motion";
 
 const buttonVariants = {
   initial: {
-    y: 0,
+    y: 0
   },
   hover: {
-    y: "0.5vw",
-  },
+    y: "5px"
+  }
 };
 
 const Button = ({
   onclick,
   contents,
-  className = "",
+  className = ""
 }: {
   onclick: () => void;
   contents: string;
@@ -25,8 +25,7 @@ const Button = ({
       onClick={onclick}
     >
       <motion.button
-        className={` top-0  border-2 border-[#F58134] rounded-full py-2 px-6 md:py-3 md:px-8 whitespace-nowrap 
-          md:text-[1.2vw] text-[4vw] min-w-[10vw] md:min-h-[3vw] min-h-[7vw] bg-[#1E1E1E] text-white
+        className={` top-0  border-2 border-[#F58134] rounded-full py-2 px-6   whitespace-nowrap  text-[20px] bg-[#1E1E1E] text-white
          `}
         variants={buttonVariants}
         initial="initial"
@@ -35,14 +34,13 @@ const Button = ({
       >
         {contents}
       </motion.button>
+      {/* Invisible element to maintain height */}
       <div
-        className={`absolute top-0 translate-y-[1.2vw] md:translate-y-[0.5vw] border-2  border-[#F58134] w-full h-full rounded-full z-[-1] bg-[#F58134]
+        className={`absolute top-0  translate-y-[5px] border-2  border-[#F58134] w-full h-full rounded-full z-[-1] bg-[#F58134]
          `}
       ></div>
-      {/* Invisible element to maintain height */}
-      <div className="absolute top-0 invisible py-2 px-6 md:py-3 md:px-8 md:text-[1.2vw] text-[4vw] ">
-        {contents}
-      </div>
+
+      <div className="absolute top-0 invisible py-2 px-6   ">{contents}</div>
     </div>
   );
 };
