722fba9840f3264cbba1df77ab20b5038704e467
Ishayu <ishayu,.ghosh@gmail.com>
2025-04-12 20:23:06 -0700

    finished cards flipping

--- src/Components/Card.tsx
+++ src/Components/Card.tsx
@@ -32,20 +32,23 @@ const LandingCard = ({ title, description, image }: CardProps) => {
             alt={title}
             className="absolute w-[80%] object-cover opacity-100 transition-opacity duration-300 left-1/2 top-[30%] transform -translate-x-1/2"
           />
-          <div className="relative z-10 text-center px-4 h-[80%] flex flex-col gap-4">
+          <div className="relative z-10 text-center px-4 h-[80%] flex flex-col gap-4 justify-start pt-6 w-full">
             <h3 className="text-[clamp(15px,2.5vw,20px)] font-bold">{title}</h3>
           </div>
         </div>
 
         {/* Back Side */}
-        <div className="absolute inset-0 flex flex-col items-center justify-center gap-4 rounded-[1.5vw] text-white bg-black px-4 [backface-visibility:hidden] [transform:rotateY(180deg)]">
-          <h3 className="text-[clamp(15px,2.5vw,20px)] font-bold">{title}</h3>
-          <p className="px-2 text-left leading-relaxed">{description}</p>
+        <div className="absolute inset-0 flex flex-col items-center justify-center gap-4 border border-white rounded-[1.5vw] text-white bg-black overflow-hidden [backface-visibility:hidden] [transform:rotateY(180deg)]">
+          <div className="relative z-10 text-center px-4 h-[80%] flex flex-col gap-4 justify-start pt-6 w-full">
+            <h3 className="text-[clamp(15px,2.5vw,20px)] font-bold">{title}</h3>
+            <p className="text-left leading-relaxed">{description}</p>
+          </div>
         </div>
       </div>
     </div>
   );
-}
+};
+
 
 
 
